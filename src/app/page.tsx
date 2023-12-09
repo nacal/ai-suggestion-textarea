46322eb4aae56a858ca699bd1fb41cd79c4242c9
nacal <nacal.dev@gmail.com>
2023-12-10 03:18:24 +0900

    tabで取り込めるように

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,24 @@ export default function Home() {
   const [suggestions, setSuggestions] = useState<string | null>(null)
   const timer = useRef<NodeJS.Timeout | null>(null)
 
+  const resetSuggestions = () => {
+    setSuggestions(null)
+  }
+
+  const caretToLast = (element: HTMLElement) => {
+    const sel = window.getSelection()
+    const range = document.createRange()
+    const textLength = element.firstChild?.textContent?.length || 0
+
+    range.setStart(element?.firstChild as Node, textLength)
+    range.collapse(true)
+
+    if (sel) {
+      sel.removeAllRanges()
+      sel.addRange(range)
+    }
+  }
+
   const handleInputValue = useCallback((e: FormEvent<HTMLDivElement>) => {
     if (timer.current) {
       clearTimeout(timer.current)
@@ -24,22 +42,36 @@ export default function Home() {
         }
       }, 1000)
     } else {
-      setSuggestions(null)
+      resetSuggestions()
     }
   }, [])
 
-  const resetSuggestions = () => {
-    setSuggestions(null)
+  const commitSuggestions = () => {
+    const textarea = document.getElementById('textbox')
+
+    if (textarea?.firstChild) {
+      textarea.firstChild.textContent = textarea.innerText
+      caretToLast(textarea)
+    }
+  }
+
+  const handleSuggestions = (e: KeyboardEvent) => {
+    if (e.key === 'Tab') {
+      e.preventDefault()
+      commitSuggestions()
+    }
+
+    resetSuggestions()
   }
 
   useEffect(() => {
     if (suggestions) {
-      addEventListener('keydown', resetSuggestions)
+      addEventListener('keydown', handleSuggestions)
       addEventListener('click', resetSuggestions)
     }
 
     return () => {
-      removeEventListener('keydown', resetSuggestions)
+      removeEventListener('keydown', handleSuggestions)
       removeEventListener('click', resetSuggestions)
     }
   }, [suggestions])
@@ -48,12 +80,14 @@ export default function Home() {
     <main className="min-h-[100dvh] flex flex-col items-center justify-center">
       <div
         contentEditable
+        suppressContentEditableWarning={true}
+        id="textbox"
         role="textbox"
         aria-label="プロフィールを入力"
         onInput={handleInputValue}
         className="w-96 h-48 outline p-4 mt-4"
       >
-        {suggestions && <span className="text-gray-400">{suggestions}</span>}
+        <span className="text-gray-400">{suggestions}</span>
       </div>
     </main>
   )
