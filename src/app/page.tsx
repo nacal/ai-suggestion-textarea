4e02269ebf110462133928e170983d2a57383b14
nacal <nacal.dev@gmail.com>
2023-12-09 23:49:47 +0900

    contentEditableに変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,51 @@
 'use client'
 
-import { FormEvent, useCallback, useRef, useState } from 'react'
+import { FormEvent, useCallback, useEffect, useRef, useState } from 'react'
 
 export default function Home() {
-  const [value, setValue] = useState('')
+  const [suggestions, setSuggestions] = useState<string | null>(null)
   const timer = useRef<NodeJS.Timeout | null>(null)
 
-  const handleInputValue = useCallback((e: FormEvent<HTMLTextAreaElement>) => {
-    setValue(e.currentTarget.value)
-
+  const handleInputValue = useCallback((e: FormEvent<HTMLDivElement>) => {
     if (timer.current) {
       clearTimeout(timer.current)
     }
 
-    if ((e.nativeEvent as InputEvent).data) {
+    if ((e.nativeEvent as InputEvent).data && e.currentTarget.textContent) {
       timer.current = setTimeout(() => {
-        console.log('fetch')
+        setSuggestions('suggestions')
       }, 1000)
+    } else {
+      setSuggestions(null)
     }
   }, [])
 
+  const resetSuggestions = () => {
+    setSuggestions(null)
+  }
+
+  useEffect(() => {
+    if (suggestions) {
+      addEventListener('keydown', resetSuggestions)
+      addEventListener('click', resetSuggestions)
+    }
+
+    return () => {
+      removeEventListener('keydown', resetSuggestions)
+      removeEventListener('click', resetSuggestions)
+    }
+  }, [suggestions])
+
   return (
     <main className="flex min-h-[100dvh] flex-col items-center justify-center">
-      <textarea
-        cols={50}
-        rows={10}
-        value={value}
+      <div
+        className="w-96 h-48 outline p-4"
+        contentEditable
         onInput={handleInputValue}
-        className="outline p-4"
-      />
+        role="textbox"
+      >
+        {suggestions && <span className="text-gray-400">{suggestions}</span>}
+      </div>
     </main>
   )
 }
